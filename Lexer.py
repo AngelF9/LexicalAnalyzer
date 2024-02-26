d738556e8e1ea300863bdb2c76a9328f29532716
Angel <angelfc@csu.fullerton.edu>
2024-02-25 18:52:16 -0800

    token types

---
+++ Lexer.py
@@ -0,0 +1,36 @@
+import re   # importing the regular expression module. allows us to work with regular expressions. 
+            # regular expressions are used to match patterns in strings.
+
+def lexicalAnalyzer(input_string):
+    # storing the keywords in a list
+    keywords = ['int', 'float', 'char', 'string', 'bool', 'if', 'else', 
+                'while', 'for', 'return', 'break', 'continue', 'switch', 
+                'case', 'default', 'void', 'main', 'print', 'input', 'true', 
+                'false', 'null']
+
+    # Defining The Token Types For The Lexems
+    # \b is used to make sure that the keyword is not a part of another word: while and NOT awhile.
+    # join is used to join all keywords with a | which means OR
+    # () allows \b to be applied to all keywords
+
+    # r is used to make the string a raw string. 
+    # In a string when using \ would need to be escaped with another \. r makes it a raw string and \ is not needed to be escaped.
+    totekn_types = {
+        'keyword': r"\b(" + "|".join(keywords) + r")\b",
+
+        # regular expression for the following:
+        'seperator': r"[();]",  # matches any single char that is () or ;
+        'identifier': r"\b[a-zA-Z][a-zA-Z0-9]*\b", 
+        'operator': r"[<>=]", 
+        'int': r"(-)?(0-9)+",
+
+        # one or more digits: \d+
+        # followed by a decimal point: \.
+        # followed by one or more digits: \d+
+        'real': r"\b\d+\.\d+\b" 
+    }
+
+    # Create a list of tuples to store the tokens
+    tokens = []
+
+    # Loop through the input string
